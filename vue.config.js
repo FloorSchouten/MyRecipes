81be8c605f35e4bf6647012fc45cd4a7fe91107f
floorschouten <70517922+FloorSchouten@users.noreply.github.com>
2023-05-16 11:42:08 +0000

    Update 3 files

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
    devServer: {
     disableHostCheck: true
+  },
+    css: {
+    loaderOptions: {
+      sass: {
+        data: `@import "@/scss/_variables.scss";`
+      }
+    }
   }
 };
